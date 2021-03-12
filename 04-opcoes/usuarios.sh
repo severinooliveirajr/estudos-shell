0b948f2b443a743b170577329a082730e6cd69c7
Severino Oliveira <severino.oliveirajr@gmail.com>
2021-03-11 23:40:32 -0300

    Adicionado suporte à pção -h

--- 04-opcoes/usuarios.sh
+++ 04-opcoes/usuarios.sh
@@ -5,7 +5,25 @@
 # Mostra os logins e nomes de usuários do sistema
 # Obs.: Lê dados do arquivo /etc/passwd
 #
+# Versão 1: Mostra usuários e nomes separados por TAB
+# Versão 2: Adicionado suporte à opção -h
+#
 # Severino, Março de 2021
 #
+MENSAGEM_USO="
+    Uso: $0 [-h]
+
+    -h Mostra esta tela de ajuda e sai
+"
+
+# Tratamento das opções da linha de comando
+if test "$1" = "-h"
+then
+    echo "$MENSAGEM_USO"
+    exit 0
+fi
+
+# Processamento
+
 cut -d : -f 1,5 /etc/passwd | tr : \\t
 
